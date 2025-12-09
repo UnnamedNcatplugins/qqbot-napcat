f44ab1fb31d694e3a3c8b04d558fb28dc2b47a33
xd <2080776216@qq.com>
2025-12-09 16:32:42 +0800

    add small func

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from pathlib import Path
 from ncatbot.core import BotClient
+from ncatbot.plugin_system import on_group_at
+from ncatbot.core.event import GroupMessageEvent
 bot = BotClient()
+
+
+@on_group_at
+async def at_func(event: GroupMessageEvent):
+    await event.reply(image=str(Path('what.jpg')))
+
+
 bot.run_frontend()
